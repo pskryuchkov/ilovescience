081971f51e10ca9969acc6601ffd37737f8e6ff7
Pavel Kryuchkov <pskryuchkov@gmail.com>
2017-11-17 20:18:54 +0300

    Notes script moved

--- notes.py
+++ notes.py
@@ -7,14 +7,14 @@ if len(argv) == 2:
     section, year, month = argv[1].split(".")
 
     if not isfile(section):
-        data = json.load(open('template.json'))
+        data = json.load(open('notes/template.json'))
         text_data = json.dumps(data)
         
         text_data = text_data.replace("{section}", section)
         text_data = text_data.replace("{year}", year.lstrip("0"))
         text_data = text_data.replace("{month}", month.lstrip("0"))
 
-        with open("{}.{}.{}.ipynb".format(section, year, month), "w") as f:
+        with open("notes/{}.{}.{}.ipynb".format(section, year, month), "w") as f:
             f.write(text_data)
 
-    nbopen.nbopen("{}.{}.{}.ipynb".format(section, year, month))
+    nbopen.nbopen("notes/{}.{}.{}.ipynb".format(section, year, month))
