f4d46480760e00aa092cb244ad84d2527c7a9dd3
Pavel Kryuchkov <pskryuchkov@gmail.com>
2018-06-21 19:33:34 +0300

    Ported to Py3

--- src/article_crawl.py
+++ src/article_crawl.py
@@ -1,25 +1,21 @@
-#!/usr/bin/python
-
 # Downloading articles from arxiv.org.
 # PDF files after loading converted to txt (on fly)
-# Usage: './load.py -s cond-mat -y 16 -m 2'
+# Usage: './article_crawl.py -s cond-mat -y 16 -m 2'
 
 # Articles saved on '../arxiv/<section>/<year>/<month>'
 
-# Tested for Anaconda Python 2.7.13
-# If script doesn't work, check your Python interpteter version.
+import logging
+from io import BytesIO
+from urllib.request import urlopen
 
-
+from pdfminer.pdfparser import PDFParser, PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from cStringIO import StringIO
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LAParams, LTTextBox, LTTextLine
 
 from os.path import isfile, join, splitext, isdir
 from os import listdir, makedirs, chdir
 import subprocess
-import urllib2
 import signal
 import sys
 import os
@@ -28,17 +24,10 @@ import argparse
 import time
 import re
 
-import socks
-import socket
-
-year = None
-month = None
-section = None
+year, month, section = None, None, None
 index_path = None
 target_url = None
-
-maxArticles = 2000
-time_lim = 40
+time_lim = 20
 
 # cn.arXiv.org (China, OK)
 # de.arXiv.org (Germany, Bad)
@@ -50,41 +39,89 @@ valid_mirrors = ["cn.", "de.", "in.", "es.", "lanl."]
 mirror = ""
 
 
-def pdf_from_url_to_txt(url):
+def pdf_to_txt(url):
+    fp = BytesIO(urlopen(url).read())
+    parser = PDFParser(fp)
+    doc = PDFDocument()
+    parser.set_document(doc)
+    doc.set_parser(parser)
+    doc.initialize('')
     rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
+    laparams = LAParams()
 
-    device = TextConverter(rsrcmgr, retstr, codec='utf-8', laparams=LAParams())
+    for param in ("all_texts", "detect_vertical", "word_margin",
+                  "char_margin", "line_margin", "boxes_flow"):
+        paramv = locals().get(param, None)
+        if paramv is not None:
+            setattr(laparams, param, paramv)
 
-    start_loading = time.time()
-    fp = StringIO(urllib2.urlopen(url).read())
-    print("load ok")
-    loading_time = time.time() - start_loading
-
+    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
     interpreter = PDFPageInterpreter(rsrcmgr, device)
-    pagenos=set()
 
-    start_processing = time.time()
-    err_flag = False
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=30, password="", caching=False,
-                                  check_extractable=True):
+    extracted_text = ''
+    return_code = 0
+    for page in doc.get_pages():
         try:
             interpreter.process_page(page)
-        except(Exception, exc):
-            if str(exc) == "Timeout":
-                print("Converting timeout: {0}".format(url))
-            else:
-                print("PDF converting error: {0}".format(url))
-            err_flag = True
+            layout = device.get_result()
+            for lt_obj in layout:
+                if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
+                    extracted_text += lt_obj.get_text()
+        except:
+            print("PDF converting error: {0}".format(url))
+            return_code = 1
             break
 
-    if not err_flag: print("convert ok")
-    text = retstr.getvalue()
-    fp.close()
-    device.close()
-    retstr.close()
-    processing_time = time.time() - start_processing
-    return text, loading_time, processing_time, err_flag
+    if return_code == 0:
+        print("convert ok")
+
+    return return_code, extracted_text
+
+
+def main():
+    signal.signal(signal.SIGALRM, handler) # func timeout set
+
+    if 'darwin' in sys.platform:
+        subprocess.Popen('caffeinate') # stay active (os x)
+
+    print("Connecting to arxiv...")
+    response = urlopen(target_url).read().decode("utf-8")
+    articles = list(set(re.findall('arXiv:({0}{1}.[0-9]+)'.format(year, str(month).zfill(2)), response)))
+    n = len(articles)
+
+    base = []
+    if isdir(index_path):
+        base = [splitext(f)[0] for f in listdir(index_path) if isfile(join(index_path, f)) and
+                                        splitext(f)[1] == ".txt"] # FIXME
+    else:
+        makedirs(index_path)
+
+    print("Loading articles...")
+    for j in range(len(articles)):
+        signal.alarm(time_lim)
+        try:
+            if articles[j] not in base:
+                link = "http://{0}arXiv.org/pdf/{1}.pdf".format(mirror, articles[j])
+
+                init_time = time.time()
+                code, text = pdf_to_txt(link)
+                if code == 0:
+                    print("{0}/{1}: {2}, {3}s".format(j + 1, n, articles[j],
+                                                      round(time.time() - init_time, 2)))
+
+                    with open('{0}/{1}.txt'.format(index_path, articles[j]), 'a') as the_file:
+                        the_file.write(text)
+            else:
+                print("{0} already loaded".format(articles[j]))
+
+            signal.alarm(0)
+
+        except Exception as exc:
+            print("{}: {}".format(articles[j], exc))
+
+
+def handler(signum, frame):
+    raise Exception("Timeout")
 
 
 def arg_run():
@@ -108,8 +145,13 @@ def arg_run():
     year = int(args.y[0])
     month = int(args.m[0])
 
-    if args.o is not None:
-        mirror = args.o[0]
+    if args.o:
+        mirror = args.o[0] + "."
+        if mirror in valid_mirrors:
+            print("Mirror: {0}".format(mirror[:-1]))
+        else:
+            print("Invalid mirror: {}".format(mirror))
+            exit()
 
     print("Section: {}, year: {}, month: {}".format(section, year, month))
 
@@ -121,84 +163,8 @@ def arg_run():
     main()
 
 
-def main():
-    global mirror
-    signal.signal(signal.SIGALRM, handler) # func timeout set
-
-    if 'darwin' in sys.platform:
-        subprocess.Popen('caffeinate') # stay active (os x)
-
-    if len(mirror) > 0:
-        if mirror in valid_mirrors:
-            print("Mirror: {0}".format(mirror))
-        elif mirror == "default":
-            mirror = ""
-        else:
-            print("Invalid mirror: {}".format(mirror))
-            exit()
-
-    print("Connecting to arxiv...")
-    data = urllib2.urlopen(target_url)
-
-    articles = []
-    for line in data:
-        result = re.findall('arXiv:({0}{1}.[0-9]+)'.format(year,
-                                                    str(month).zfill(2)), line)
-        if result != []:
-            articles.append(result[0])
-
-    articles = list(set(articles))
-    n = len(articles)
-
-    base = []
-    if isdir(index_path):
-        base = [splitext(f)[0] for f in listdir(index_path) if isfile(join(index_path, f)) and
-                                        splitext(f)[1] == ".txt"] # FIXME
-    else:
-        makedirs(index_path)
-
-    print("Loading articles...")
-    for j in range(min(len(articles), maxArticles)):
-        signal.alarm(time_lim)
-        try:
-            if articles[j] not in base:
-                link = "http://{0}arXiv.org/pdf/{1}.pdf".format(mirror, articles[j])
-
-                text, load_time, convert_time, err_flag = pdf_from_url_to_txt(link)
-
-                if not err_flag:
-                    print("{0}/{1}: {2}, {3}s / {4}s".format(j + 1,
-                                                        n,
-                                                        articles[j],
-                                                        round(load_time, 2),
-                                                        round(convert_time, 2)))
-
-                    with open('{0}/{1}.txt'.format(index_path, articles[j]), 'a') as the_file:
-                        the_file.write(text)
-            else:
-                print("{0} already loaded".format(articles[j]))
-
-            signal.alarm(0)
-
-        except(Exception, exc):
-            print("{}: {}".format(articles[j], exc))
-
-
-def handler(signum, frame):
-    raise Exception("Timeout")
-
-
-def create_connection(address, timeout=None, source_address=None):
-    sock = socks.socksocket()
-    sock.connect(address)
-    return sock
-
-socks.socksocket().setproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050)
-
-# patch the socket module
-socket.socket = socks.socksocket
-socket.create_connection = create_connection
-
 if __name__ == "__main__":
     chdir(os.path.dirname(os.path.realpath(__file__)))
+    logging.getLogger().setLevel(logging.ERROR)
+    logging.propagate = False
     arg_run()
